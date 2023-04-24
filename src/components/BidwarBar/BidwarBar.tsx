6e8d37284b54c2f7493a98fef93f99ca45812b76
GardonFrais <gardonfrais@proton.me>
2023-04-24 16:38:57 +0200

    API calls

--- src/components/BidwarBar/BidwarBar.tsx
+++ src/components/BidwarBar/BidwarBar.tsx
@@ -1,28 +1,54 @@
-import { useMemo, useState } from 'react';
-import './Bidwar.css'
+import { useEffect, useMemo, useState } from 'react';
+import './BidwarBar.css';
+
+const BIDWAR_ENDPOINT = `${import.meta.env.VITE_API_BASE_URL}/bidwar`;
 
 const BidwarBar: React.FC = () => {
-  const [montantVerlan, setMontantVerlan] = useState(9000);
-  const [montantAnglais, setMontantAnglais] = useState(4500);
 
-  const getVerlanPourcentage = useMemo(() => {
-    return 100 * montantVerlan / (montantVerlan + montantAnglais) + '%'
-  }, [montantVerlan, montantAnglais])
+  const searchParams = new URLSearchParams(document.location.search);
+  const user = searchParams.get('user');
+  const firstIncentive = searchParams.get('firstIncentive');
+  const secondIncentive = searchParams.get('secondIncentive');
 
-  const getAnglaisPourcentage = useMemo(() => {
-    return 100 * montantAnglais / (montantVerlan + montantAnglais) + '%'
-  }, [montantVerlan, montantAnglais])
+  const [firstAmount, setFirstAmount] = useState(0);
+  const [secondAmount, setSecondAmount] = useState(0);
+
+  const getFirstPercentage = useMemo(() => {
+    return 100 * firstAmount / (firstAmount + secondAmount) + '%';
+  }, [firstAmount, secondAmount]);
+
+  const getSecondPourcentage = useMemo(() => {
+    return 100 * secondAmount / (firstAmount + secondAmount) + '%';
+  }, [firstAmount, secondAmount]);
+
+  useEffect(() => {
+    const INTERVAL_TIME_MS = 15000;
+    const getBidwarDatas = () => {
+      fetch(`${BIDWAR_ENDPOINT}/${user}/${firstIncentive}/${secondIncentive}`)
+      .then(response => response.json())
+      .then(json => {
+        setFirstAmount(json.firstResult.amount);
+        setSecondAmount(json.secondResult.amount);
+      })
+      .catch(error => console.error(error));
+    }
+    
+    getBidwarDatas();
+    const interval = setInterval(getBidwarDatas, INTERVAL_TIME_MS);
+
+    return () => clearInterval(interval);
+  }, [user, firstIncentive, secondIncentive]);
 
   return (
    <div className='bidwar-bar'>
-      <div className='verlan' style={{width: getVerlanPourcentage}}>
-        <span className='label'>#VERLAN {montantVerlan}€</span>
+      <div className='first-incentive' style={{width: getFirstPercentage}}>
+        <span className='label'>#{firstIncentive?.toUpperCase()} {firstAmount}€</span>
       </div>
-      <div className='anglais' style={{width: getAnglaisPourcentage}}>
-        <span className='label'>#ANGLAIS {montantAnglais}€</span>
+      <div className='second-incentive' style={{width: getSecondPourcentage}}>
+        <span className='label'>#{secondIncentive?.toUpperCase()} {secondAmount}€</span>
       </div>
     </div>  
-  )
+  );
 }
 
-export default BidwarBar
+export default BidwarBar;
